8906f9eec780b80d7b6578595584e23bef7211d1
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-04-07 15:38:00 +0300

    Render template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 # Create a function that will display
 # the 'Hello World string.'
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello World!'
+	return render_template('index.html')
