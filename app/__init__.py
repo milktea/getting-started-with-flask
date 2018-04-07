84ca4bd011469dfc200c428c514adeb6aaab2a85
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-04-07 13:37:04 +0300

    Add the __init__.py module

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+# Import the Flask class.
+# Create an instance of it.
+# Pass in the __name__ arguement,
+# to indicate the appplication's module.
+# Such that Flask knows where to find other
+# files such as templates.
+from flask import Flask
+
+app = Flask(__name__)
+
+from app import views
